fd20c26489a7141986ff1099f9e10a5b343c9b74
mgalus <mgalus@dreamlab.pl>
2021-05-22 21:33:44 +0200

    1199388171576906

--- modules/backend/views/oeiizk/training/_script.php
+++ modules/backend/views/oeiizk/training/_script.php
@@ -5,32 +5,68 @@ use yii\helpers\Url;
 ?>
 <script>
   function addRow<?= $class ?>() {
+
+    ////old version
+    //var data = [];
+    //var data = $('#add-<?//= $relID?>// :input').serializeArray();
+    //data.push({ name: '_action', value: 'add' });
+    //$.ajax({
+    //  type: 'POST',
+    //  url: '<?php //echo Url::to(['add-' . $relID]); ?>//',
+    //  data: data,
+    //  success: function (data) {
+    //    //var oldTbody = $('#add-<?////= $relID?>//// tbody').html();
+    //    //var maxIndex = 0;
+    //    //$('.kv-tabform-row').each(function () {
+    //    //  if ($(this).data('key') > maxIndex) {
+    //    //    maxIndex = $(this).data('key');
+    //    //  }
+    //    //});
+    //    //maxIndex++;
+    //    //data = data.replace(/\[0\]/g, '[' + maxIndex + ']')
+    //    //            .replace('seq="0">1</td>','seq="0">'+(maxIndex+1)+'</td>')
+    //    //            .replace(/\(0\)/g, '(' + maxIndex + ')');
+    //    //data = data.replace('data-key="0"', 'data-key="' + maxIndex + '"');
+    //    //data = data.replace(/-0-/g, '-' + maxIndex + '-');
+    //    //
+    //    //var newRecord = $(data).find('tbody').html();
+    //    $('#add-<?//= $relID?>//').html(data);
+    //  },
+    //});
+
+
+
+
+    ////new faster version
     var data = [];
-    var data = $('#add-<?= $relID?> :input').serializeArray();
+    //var data = $('#add-<?= $relID?> :input').serializeArray();
     data.push({ name: '_action', value: 'add' });
     $.ajax({
       type: 'POST',
       url: '<?php echo Url::to(['add-' . $relID]); ?>',
       data: data,
       success: function (data) {
-        //var oldTbody = $('#add-<?//= $relID?>// tbody').html();
-        //var maxIndex = 0;
-        //$('.kv-tabform-row').each(function () {
-        //  if ($(this).data('key') > maxIndex) {
-        //    maxIndex = $(this).data('key');
-        //  }
-        //});
-        //maxIndex++;
-        //data = data.replace(/\[0\]/g, '[' + maxIndex + ']')
-        //            .replace('seq="0">1</td>','seq="0">'+(maxIndex+1)+'</td>')
-        //            .replace(/\(0\)/g, '(' + maxIndex + ')');
-        //data = data.replace('data-key="0"', 'data-key="' + maxIndex + '"');
-        //data = data.replace(/-0-/g, '-' + maxIndex + '-');
-        //
-        //var newRecord = $(data).find('tbody').html();
-        $('#add-<?= $relID?>').html(data);
+        var oldTbody = $('#add-<?= $relID?> tbody').html();
+        var maxIndex = 0;
+        $('.kv-tabform-row').each(function () {
+          if ($(this).data('key') > maxIndex) {
+            maxIndex = $(this).data('key');
+          }
+        });
+        maxIndex++;
+        data = data.replace(/\[0\]/g, '[' + maxIndex + ']')
+        .replace('seq="0">1</td>','seq="0">'+(maxIndex+1)+'</td>')
+        .replace(/\(0\)/g, '(' + maxIndex + ')');
+        data = data.replace('data-key="0"', 'data-key="' + maxIndex + '"');
+        data = data.replace(/-0-/g, '-' + maxIndex + '-');
+        data = data.replace(/_0/g, '_' + maxIndex);
+
+        var newRecord = $(data).find('tbody').html();
+        $('#add-<?= $relID?> tbody').html(oldTbody + newRecord);
       },
     });
+
+
   }
 
   function delRow<?= $class ?>(id) {
