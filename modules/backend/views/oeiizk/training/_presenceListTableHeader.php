a9645e4b6a0d490aeaff8f0d83bf651d49036adf
mgalus <mgalus@dreamlab.pl>
2021-06-10 22:06:53 +0200

    1199207042371526

--- modules/backend/views/oeiizk/training/_presenceListTableHeader.php
+++ modules/backend/views/oeiizk/training/_presenceListTableHeader.php
@@ -1,11 +1,13 @@
-﻿<tr>
-    <td width="4%">L.p.</td>
-    <td width="25%">Nazwisko i imię</td>
-    <td>Miejsce zatrudnienia</td>
-    <? foreach ($model->lessonsDateAsc as $lesson): ?>
-        <td width="9%">
-            <input type="hidden" name="lessonPresenceToDelete[]" value="<?= $lesson->id ?>"/>
-            <?= date('d.m.Y', strtotime($lesson->date_start)) ?>
-        </td>
-    <? endforeach ?>
-</tr>
+﻿<thead>
+    <tr>
+        <td width="4%">L.p.</td>
+        <td width="25%">Nazwisko i imię</td>
+        <td>Miejsce zatrudnienia</td>
+        <? foreach ($model->lessonsDateAsc as $lesson): ?>
+            <td width="9%">
+                <input type="hidden" name="lessonPresenceToDelete[]" value="<?= $lesson->id ?>"/>
+                <?= date('d.m.Y', strtotime($lesson->date_start)) ?>
+            </td>
+        <? endforeach ?>
+    </tr>
+</thead>
