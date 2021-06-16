fd39e0f58445f3a3ad4a18cd18dd67eda17556ef
mgalus <mgalus@dreamlab.pl>
2021-06-16 20:42:36 +0200

    1199207042371548

--- modules/backend/views/oeiizk/training/_listTableHeader.php
+++ modules/backend/views/oeiizk/training/_listTableHeader.php
@@ -4,7 +4,7 @@
 use yii\base\View;
 
 ?>
-
+<thead>
   <tr>
     <td width="4%">L.p.</td>
     <td width="25%">Nazwisko i imię</td>
@@ -12,3 +12,4 @@ use yii\base\View;
     <td width="15%"> Pokwitowanie - materiały </td>
     <td width="15%"> Pokwitowanie - zaświadczenie </td>
   </tr>
+</thead>
